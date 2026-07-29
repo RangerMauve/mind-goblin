ee5b90aa42a9fd7dbb4608f2c6d7e4902884f827
Mauve Signweaver <rangermauve@hotmail.com>
2026-07-28 20:25:56 -0400

    feat: tab completion for file paths

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import readline from 'node:readline/promises'
+import path from 'node:path'
+import fs from 'node:fs/promises'
 import { stdin as input, stdout as output } from 'node:process'
 
 import { program } from 'commander'
@@ -57,7 +59,11 @@ await program.parseAsync(process.argv)
 async function repl (options) {
   const goblin = await Goblin.fromOptions({ ...program.opts(), ...options })
 
-  const rl = readline.createInterface({ input, output })
+  const rl = readline.createInterface({
+    input,
+    output,
+    completer
+  })
 
   /**
    * @type {import('./index.js').Message[]}
@@ -90,10 +96,49 @@ async function repl (options) {
       await confirm(`Allow shell command?\n${args.command}`)
     }
     if (name === 'write_file') {
-      await confirm(`Allow write to ${args.path}?\nContent: ${args.content}`)
+      await confirm(`Allow write to ${args.path}?\nContent:\n${args.content}`)
     }
     if (name === 'edit_file') {
-      await confirm(`Allow edit to ${args.path}?\nReplace: ${args.old_text}\nWith: ${args.new_text}`)
+      await confirm(`Allow edit to ${args.path}?\nReplace:\n${args.old_text}\nWith:\n${args.new_text}`)
+    }
+  }
+
+  /**
+   * @param {string} line
+   */
+  async function completer (line) {
+    const parts = line.split(' ')
+    const lastPart = parts[parts.length - 1]
+    // Only add the trailing space if there's more than one part
+    // Else we should leave the first part blank
+    const trailingSpace = (parts.length > 1 ? ' ' : '')
+    const firstPart = parts.slice(0, -1).join(' ') + trailingSpace
+
+    if (!lastPart.startsWith('./') && !lastPart.startsWith('/')) {
+      return [[], line]
+    }
+
+    try {
+      const isFolder = lastPart.endsWith('/')
+      const fullPath = path.join(process.cwd(), lastPart)
+
+      const dir = isFolder ? fullPath : path.dirname(fullPath)
+      const base = isFolder ? '' : path.basename(fullPath).toLowerCase()
+      // Only add the last part's base name if it isn't a folder
+      const prefix = firstPart + (isFolder ? lastPart : lastPart.slice(0, -base.length))
+
+      // Get the fils and folders from the dir
+      const files = await fs.readdir(dir, { withFileTypes: true })
+      // Case insensitive match
+      const matches = files.filter(f => f.name.toLowerCase().startsWith(base))
+
+      // Add the trailing slash if it's a folder to trigger further completions
+      const completed = matches.map(m => prefix + m.name + (m.isDirectory() ? '/' : ''))
+      // console.log({lastPart, firstPart, dir, base, prefix, completed})
+      return [completed, line]
+    } catch (e) {
+      console.error(e)
+      return [[], line]
     }
   }
 
